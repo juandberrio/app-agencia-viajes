c49de3daa2f4fb23ca699fcbf5f1ff67734d34bc
lucho <lmrivas2006@gmail.com>
2025-03-18 18:20:16 -0500

    creacion proyecto

---
+++ src/PanelControl.jsx
@@ -0,0 +1,24 @@
+function PanelControl(){
+    return(
+        <div className="panel-control">
+            <aside className="panel-control-aside">
+                <div>
+                    <img src="" alt="" />
+                </div>
+                <nav>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                                                  
+                    </ul>
+                </nav>
+            </aside>
+            <section className="panel-control-section">
+                <h1>Mensaje de Bienvenida</h1>
+            </section>
+        </div>
+    )
+}
+export default PanelControl;
